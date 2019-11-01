9f37e2aaa2b1b93ecc7a0c997a3dcb1dd6befaba
linliangbin <linliangbin@91prize.com>
2019-11-01 20:24:32 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,28 @@
+# coding=utf-8
+import json
+import time
+from hashlib import sha256
+
+class Block:
+    def __init__(self, index, transactions, timestamp, previous_hash):
+        self.index = index
+        self.transactions = transactions
+        self.timestamp = timestamp
+        self.previous_hash = previous_hash
+
+    def compute_hash(self):
+        block_string = json.dumps(self.__dict__, sort_keys=True)
+        return sha256(block_string.encode()).hexdigest()
+
+
+class Blockchain:
+    def __init__(self):
+        self.unconfirmed_transactions = []
+        self.chain = []
+        self.create_genesis_block()
+
+    def create_genesis_block(self):
+        genesis_block = Block(0, [], time.time(), "0")
+        genesis_block.hash = genesis_block.compute_hash()
+        self.chain.append(genesis_block)
+    
